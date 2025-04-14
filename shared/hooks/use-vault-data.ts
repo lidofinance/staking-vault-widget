76e44f85ed6d75090163fb50f0acaf8b1973bc59
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2025-04-14 14:56:16 +0300

    feat: make updates in useVaultData after review

--- shared/hooks/use-vault-data.ts
+++ shared/hooks/use-vault-data.ts
@@ -9,6 +9,7 @@ import { STRATEGY_LAZY } from 'consts/react-query-strategies';
 import { getHealthScore } from 'utils/get-health-score';
 import { VaultSocket, VaultInfo } from 'types';
 import { usePublicClient } from 'wagmi';
+import invariant from 'tiny-invariant';
 
 // TODO: find way to remove readonly
 export const useVaultData = (
@@ -22,13 +23,10 @@ export const useVaultData = (
     enabled: !!vaultsAddressesList?.length && !!publicClient,
     ...STRATEGY_LAZY,
     queryFn: async (): Promise<VaultInfo[]> => {
+      invariant(publicClient, 'PublicClient is not ready');
+
+      const vaultHubContract = getVaultHubContract(publicClient);
       const vaults: VaultInfo[] = [];
-
-      if (!publicClient) {
-        return vaults;
-      }
-
-      const vaultHabContract = getVaultHubContract(publicClient);
 
       if (vaultsAddressesList?.length && vaultsAddressesList.length === 0) {
         return vaults;
@@ -40,35 +38,45 @@ export const useVaultData = (
           vaultAddress,
           publicClient,
         );
-        const owner = await vaultContract.read.owner();
-        const inOutDelta = await vaultContract.read.inOutDelta();
-        const nodeOperator = await vaultContract.read.nodeOperator();
+
+        const [owner, inOutDelta, nodeOperator, locked] = await Promise.all([
+          vaultContract.read.owner(),
+          vaultContract.read.inOutDelta(),
+          vaultContract.read.nodeOperator(),
+          vaultContract.read.locked(),
+        ]);
+
         const balance = await publicClient.getBalance({
           address: vaultContract.address,
         });
-        const locked = await vaultContract.read.locked();
-        const delegationContract = getDelegationContract(owner, publicClient);
 
         const vaultHubSocket: VaultSocket =
-          await vaultHabContract.read.vaultSocket([vaultAddress]);
-        const valuation = await delegationContract.read.valuation();
-        const nodeOperatorUnclaimedFee =
-          await delegationContract.read.nodeOperatorUnclaimedFee();
-        const withdrawableEther =
-          await delegationContract.read.withdrawableEther();
+          await vaultHubContract.read.vaultSocket([vaultAddress]);
+        const delegationContract = getDelegationContract(owner, publicClient);
 
-        const nodeOperatorFeeBP =
-          await delegationContract.read.nodeOperatorFeeBP();
+        const [
+          valuation,
+          nodeOperatorUnclaimedFee,
+          withdrawableEther,
+          nodeOperatorFeeBP,
+          totalMintableShares,
+        ] = await Promise.all([
+          delegationContract.read.valuation(),
+          delegationContract.read.nodeOperatorUnclaimedFee(),
+          delegationContract.read.withdrawableEther(),
+          delegationContract.read.nodeOperatorFeeBP(),
+          delegationContract.read.totalMintableShares(),
+        ]);
+
+        const [mintedEth, mintableEth, ethLimit] = await Promise.all([
+          shares.convertToSteth(vaultHubSocket.sharesMinted),
+          shares.convertToSteth(
+            totalMintableShares - vaultHubSocket.sharesMinted,
+          ),
+          shares.convertToSteth(vaultHubSocket.shareLimit),
+        ]);
+
         const healthScore = getHealthScore(valuation, vaultHubSocket);
-        const totalMintableShares =
-          await delegationContract.read.totalMintableShares();
-        const mintedEth = await shares.convertToSteth(
-          vaultHubSocket.sharesMinted,
-        );
-        const mintableEth = await shares.convertToSteth(
-          totalMintableShares - vaultHubSocket.sharesMinted,
-        );
-        const ethLimit = await shares.convertToSteth(vaultHubSocket.shareLimit);
 
         vaults.push({
           mintable: mintableEth,
