a3225754bad59642a8cb33b594d62536d3ec2524
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2025-04-24 13:29:54 +0300

    fix: remove overlogic statement

--- features/home/home-content/home-content.tsx
+++ features/home/home-content/home-content.tsx
@@ -1,16 +1,14 @@
 import { useRouter } from 'next/router';
 import { MyVaults } from 'features/home/my-vaults';
 import { AllVaults } from 'features/home/all-vaults';
-import { AppPaths } from 'consts/urls';
 
 export const HomeContent = () => {
   const router = useRouter();
   const {
-    pathname,
     query: { mode },
   } = router;
 
-  const showPersonal = pathname === AppPaths.main && mode === 'personal';
+  const showPersonal = mode === 'personal';
 
   return <>{showPersonal ? <MyVaults /> : <AllVaults />}</>;
 };
