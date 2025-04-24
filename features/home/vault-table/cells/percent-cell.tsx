630d6d05759f9ec2f841cc28f4c57b197db4fe6d
DiRaiks <t.fane@mail.ru>
2025-04-24 13:03:29 +0300

    fix: update percent calculation in PercentCell and adjust health score return value

--- features/home/vault-table/cells/percent-cell.tsx
+++ features/home/vault-table/cells/percent-cell.tsx
@@ -4,11 +4,9 @@ import { Percent } from './styles';
 
 import { formatPercent } from 'utils/format-number';
 import { BaseCellProps } from '../types';
-import { VAULT_TOTAL_BASIS_POINTS } from 'modules/vaults';
 
 export const PercentCell: FC<BaseCellProps<number>> = ({ value }) => {
-  const valuePercent = value / VAULT_TOTAL_BASIS_POINTS;
-  const percent = formatPercent.format(valuePercent);
+  const percent = formatPercent.format(value);
 
   return <Percent value={value}>{percent}</Percent>;
 };
