5a448aee5723e59cfa94de5cdd0a71d8bf2f0119
Evgeny Taktarov <je.mouth@gmail.com>
2025-05-07 16:28:09 +0700

    feat: claim fee flow

--- features/claim/claim-form/claim-form-context/claim-form-provider.tsx
+++ features/claim/claim-form/claim-form-context/claim-form-provider.tsx
@@ -5,13 +5,11 @@ import {
   useCallback,
   createContext,
   useContext,
-  useState,
 } from 'react';
 import { isAddress, ReadContractErrorType } from 'viem';
 import { useReadContract } from 'wagmi';
 import { FormProvider, useForm } from 'react-hook-form';
 
-import { useFormControllerRetry } from 'shared/hook-form/form-controller/use-form-controller-retry-delegate';
 import { useClaim } from 'features/claim/claim-form/hooks';
 import { useVaultInfo } from 'modules/vaults';
 
@@ -24,11 +22,6 @@ import {
 import { dashboardAbi } from 'abi/dashboard-abi';
 import { ClaimFormSchema } from 'features/claim/claim-form/types';
 import invariant from 'tiny-invariant';
-import {
-  SubmitPayload,
-  SubmitStepEnum,
-} from 'shared/components/submit-modal/types';
-import { SubmitModal } from 'shared/components';
 
 type ClaimDataContextValue = {
   availableToClaim: bigint | undefined;
@@ -53,10 +46,27 @@ export const useClaimFormData = () => {
 export const ClaimFormProvider: FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [submitStep, setSubmitStep] = useState<SubmitPayload>(() => ({
-    step: SubmitStepEnum.edit,
-  }));
   const { activeVault } = useVaultInfo();
+
+  const formObject = useForm<ClaimFormSchema>({
+    defaultValues: {
+      recipient: '',
+    },
+    mode: 'all',
+    // TODO: validation
+    reValidateMode: 'onChange',
+  });
+  const { claim, retryEvent } = useClaim();
+
+  const onSubmit = useCallback(
+    async ({ recipient }: ClaimFormSchema) => {
+      // TODO: add validation, remove stub
+      if (!isAddress(recipient)) return false;
+
+      return claim(recipient);
+    },
+    [claim],
+  );
 
   const {
     data: availableToClaim,
@@ -82,55 +92,14 @@ export const ClaimFormProvider: FC<{ children: ReactNode }> = ({
     [availableToClaim, isLoadingClaimInfo, isErrorClaimInfo, errorClaimInfo],
   );
 
-  const formObject = useForm<ClaimFormSchema>({
-    defaultValues: {
-      recipient: '',
-    },
-    mode: 'all',
-    reValidateMode: 'onChange',
-  });
-  const { callClaim } = useClaim();
-
-  const { retryEvent, retryFire } = useFormControllerRetry();
-  const setModalState = useCallback((submitStep: SubmitPayload) => {
-    setSubmitStep(submitStep);
-  }, []);
-
-  const onSubmit = useCallback(
-    async ({ recipient }: ClaimFormSchema) => {
-      if (recipient && isAddress(recipient)) {
-        try {
-          setModalState({ step: SubmitStepEnum.initiate });
-          const tx = await callClaim(recipient, setModalState);
-          setModalState({ step: SubmitStepEnum.overview, tx });
-          return true;
-        } catch (err) {
-          if (
-            err instanceof Error &&
-            err.message.includes('User rejected the request')
-          ) {
-            setModalState({ step: SubmitStepEnum.reject });
-          } else {
-            setModalState({ step: SubmitStepEnum.error });
-          }
-        }
-      }
-
-      return false;
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [callClaim],
-  );
-
   const formControllerValue: FormControllerContextValueType<ClaimFormSchema> =
     useMemo(
       () => ({
         onSubmit,
         retryEvent,
-        retryFire,
         onReset: formObject.reset,
       }),
-      [retryFire, retryEvent, onSubmit, formObject.reset],
+      [retryEvent, onSubmit, formObject.reset],
     );
 
   return (
@@ -138,7 +107,6 @@ export const ClaimFormProvider: FC<{ children: ReactNode }> = ({
       <FormProvider {...formObject}>
         <FormControllerContext.Provider value={formControllerValue}>
           <FormController>{children}</FormController>
-          <SubmitModal submitStep={submitStep} setModalState={setModalState} />
         </FormControllerContext.Provider>
       </FormProvider>
     </ClaimDataContext.Provider>
