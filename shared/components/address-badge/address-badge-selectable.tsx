985f55ea097ef46c82ddabcfd51445fdc882aa05
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2025-05-15 19:39:43 +0300

    fix: fix component type

--- shared/components/address-badge/address-badge-selectable.tsx
+++ shared/components/address-badge/address-badge-selectable.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ChangeEvent, useId, forwardRef } from 'react';
+import { ChangeEvent, useId, forwardRef } from 'react';
 import { AddressBadge, AddressBadgeProps } from 'shared/components/index';
 import { Close } from '@lidofinance/lido-ui';
 import { ReactComponent as RestoreIcon } from 'assets/icons/restore.svg';
@@ -12,37 +12,34 @@ export interface SelectableAddressBadgeProps
   defaultBg?: AddressBadgeProps['bgColor'];
 }
 
-export const AddressBadgeSelectable: FC<SelectableAddressBadgeProps> =
-  forwardRef<HTMLDivElement | null, SelectableAddressBadgeProps>(
-    (
-      { checked, onCheckedChange, defaultBg = 'default', ...badgeProps },
-      ref,
-    ) => {
-      const id = useId();
+export const AddressBadgeSelectable = forwardRef<
+  HTMLDivElement | null,
+  SelectableAddressBadgeProps
+>(({ checked, onCheckedChange, defaultBg = 'default', ...badgeProps }, ref) => {
+  const id = useId();
 
-      const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        onCheckedChange(e.target.checked);
-      };
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onCheckedChange(e.target.checked);
+  };
 
-      return (
-        <SelectableWrapper crossed={checked} bgColor={defaultBg}>
-          <AddressBadge
-            ref={ref}
-            {...badgeProps}
-            bgColor="transparent"
-            crossed={checked}
-          />
+  return (
+    <SelectableWrapper crossed={checked} bgColor={defaultBg}>
+      <AddressBadge
+        ref={ref}
+        {...badgeProps}
+        bgColor="transparent"
+        crossed={checked}
+      />
 
-          <Label htmlFor={id}>
-            <HiddenCheckbox
-              id={id}
-              type="checkbox"
-              checked={checked}
-              onChange={handleInputChange}
-            />
-            {checked ? <RestoreIcon /> : <Close />}
-          </Label>
-        </SelectableWrapper>
-      );
-    },
+      <Label htmlFor={id}>
+        <HiddenCheckbox
+          id={id}
+          type="checkbox"
+          checked={checked}
+          onChange={handleInputChange}
+        />
+        {checked ? <RestoreIcon /> : <Close />}
+      </Label>
+    </SelectableWrapper>
   );
+});
