820490a4d0bb0ced5bf0cad3f54ba0cdbcfa7d35
Evgeny Taktarov <je.mouth@gmail.com>
2025-06-06 15:53:46 +0700

    chore: restore code

--- utils/validate-form-value.ts
+++ utils/validate-form-value.ts
@@ -1,9 +1,56 @@
 import { z, ZodSchema } from 'zod';
 import { type Address, isAddress } from 'viem';
-import { Resolver } from 'react-hook-form';
+import { appendErrors, FieldError, Resolver } from 'react-hook-form';
 
 import { isZodError } from 'utils/errors';
 import { vaultTexts } from 'modules/vaults';
+
+export const parseZodErrorSchema = (
+  zodErrors: z.ZodIssue[],
+  validateAllFieldCriteria: boolean,
+) => {
+  const errors: Record<string, FieldError> = {};
+  for (const error of zodErrors) {
+    const { code, message, path } = error;
+    const _path = path.join('.');
+
+    if (!errors[_path]) {
+      if ('unionErrors' in error) {
+        const unionError = error.unionErrors[0].errors[0];
+
+        errors[_path] = {
+          message: unionError.message,
+          type: unionError.code,
+        };
+      } else {
+        errors[_path] = { message, type: code };
+      }
+    }
+
+    if ('unionErrors' in error) {
+      error.unionErrors.forEach((unionError) =>
+        unionError.errors.forEach((e) => zodErrors.push(e)),
+      );
+    }
+
+    if (validateAllFieldCriteria) {
+      const types = errors[_path].types;
+      const messages = types && types[error.code];
+
+      errors[_path] = appendErrors(
+        _path,
+        validateAllFieldCriteria,
+        errors,
+        code,
+        messages
+          ? ([] as string[]).concat(messages as string[], error.message)
+          : error.message,
+      ) as FieldError;
+    }
+  }
+
+  return errors;
+};
 
 // TODO: recheck with lib to remove this code
 // https://react-hook-form.com/docs/useform#:~:text=of%20your%20fields.-,Examples%3A,-YUP
