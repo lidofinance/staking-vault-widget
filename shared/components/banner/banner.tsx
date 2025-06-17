316d9c458e06ffc2ed3cba2b695230dd2fe86051
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2025-06-17 16:24:11 +0300

    refactor: remove link in MigrationBannerTestnetV2

--- shared/components/banner/banner.tsx
+++ shared/components/banner/banner.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 import { BannerWrapper, TextWhite } from './styles';
 
 export const MigrationBannerTestnetV2 = () => {
@@ -7,15 +5,8 @@ export const MigrationBannerTestnetV2 = () => {
     <BannerWrapper>
       <TextWhite>
         All Testnet data will not be available via Web UI on 25 Jun 2025 as part
-        of migration to the&nbsp;
-        <Link
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://docs.lido.fi/deployed-contracts/hoodi-lidov3"
-        >
-          stVaults Testnet-2 contracts.
-        </Link>
-        &nbsp;Back up anything important.
+        of migration to the stVaults Testnet-2 contracts. Back up anything
+        important.
       </TextWhite>
     </BannerWrapper>
   );
