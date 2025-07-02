bb3c0eacaeb8d301e90f6984f081765fd50aabc2
Evgeny Taktarov <je.mouth@gmail.com>
2025-07-02 15:34:42 +0700

    fix: revert ipfs

--- modules/vaults/report/ipfs/ipfs-gateways.ts
+++ modules/vaults/report/ipfs/ipfs-gateways.ts
@@ -1,5 +1,5 @@
 export const CID_TO_GATEWAY: ((cid: string) => string)[] = [
-  (cid) => `https://${cid}.ipfs.dweb.link/`,
+  (cid) => `https://dweb.link/ipfs/${cid}`,
   (cid) => `https://ipfs.io/ipfs/${cid}`,
   (cid) => `https://gateway.pinata.cloud/ipfs/${cid}`,
 ];
