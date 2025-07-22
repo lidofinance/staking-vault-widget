7d872c6288ed931f4f4722c97fb0d5a00975f09f
Evgeny Taktarov <je.mouth@gmail.com>
2025-07-22 16:10:32 +0200

    fix: table overflow

--- features/home/styles.ts
+++ features/home/styles.ts
@@ -1,14 +1,8 @@
 import styled from 'styled-components';
 
-import { Container } from '@lidofinance/lido-ui';
-
-export const PageWrapper = styled(Container)`
+export const PageWrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
-  width: 100%;
   gap: ${({ theme }) => theme.spaceMap.md}px;
   margin-top: ${({ theme }) => theme.spaceMap.xxl}px;
-  padding: 0;
 `;
