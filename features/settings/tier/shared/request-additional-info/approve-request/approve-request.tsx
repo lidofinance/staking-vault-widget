435e8f1edcceb9c2b8fce16a7fe55a3d94df9dee
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2025-08-29 16:32:05 +0300

    fix: hide approve button for non owner and node operator addresses

--- features/settings/tier/shared/request-additional-info/approve-request/approve-request.tsx
+++ features/settings/tier/shared/request-additional-info/approve-request/approve-request.tsx
@@ -3,12 +3,24 @@ import { useCallback } from 'react';
 import { useChangeTierRequest, useVaultTierInfo } from '../../../hooks';
 
 import { ButtonStyled } from './styles';
+import { useAccount } from 'wagmi';
+import {
+  useVault,
+  useVaultConfirmingRoles,
+} from '../../../../../../modules/vaults';
 
 export const ApproveRequest = () => {
   const { approveMovingTier, approving } = useChangeTierRequest();
   const { data: vaultTierInfo, refetch } = useVaultTierInfo();
+  const { address } = useAccount();
+  const { activeVault } = useVault();
+  const { hasConfirmingRole } = useVaultConfirmingRoles();
 
   const proposal = vaultTierInfo?.proposals.lastProposal;
+  const hasAccessToApproving =
+    activeVault?.nodeOperator === address ||
+    hasConfirmingRole ||
+    proposal?.member?.toLowerCase() !== address?.toLowerCase();
 
   const handleApprove = useCallback(async () => {
     const [_, tierId, mintingLimit] = proposal?.decodedData.args ?? [];
@@ -18,7 +30,7 @@ export const ApproveRequest = () => {
     await refetch();
   }, [approveMovingTier, refetch, proposal]);
 
-  if (!proposal) return null;
+  if (!proposal || !hasAccessToApproving) return null;
 
   return (
     <ButtonStyled onClick={handleApprove} disabled={approving}>
