286eb5d191a92ebd0a17c8369b2169e3e9e672fb
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2025-10-13 12:22:29 +0300

    feat: remove devnet settings

--- config/network/index.ts
+++ config/network/index.ts
@@ -1,43 +1,16 @@
-import invariant from 'tiny-invariant';
-import getConfigNext from 'next/config';
 import type { Address } from 'viem';
 
 import { CHAINS } from '@lidofinance/lido-ethereum-sdk/common';
 
-import { getPreConfig } from 'config/get-preconfig';
 import type { API_NAMES, CONTRACT_NAMES, NetworkConfig } from './const';
 
 // Main deployments
 import mainnetSet from 'networks/mainnet.json' assert { type: 'json' };
 import hoodiSet from 'networks/hoodi.json' assert { type: 'json' };
 
-// Testnet deployments
-import hoodiPublic1Set from 'networks/hoodi-public-1.json' assert { type: 'json' };
-
 // reexport types and const
 export type { CONTRACT_NAMES, NetworkConfig } from './const';
 export { CONTRACTS } from './const';
-
-const { serverRuntimeConfig } = getConfigNext();
-
-const DEVNET_OVERRIDES: Record<number, string> = // Merge client&server values
-  (serverRuntimeConfig.devnetOverrides || getPreConfig().devnetOverrides || '')
-    .split(',')
-    .reduce(
-      (acc, override) => {
-        const [chainId, setName] = override.split(':');
-        if (!isNaN(Number(chainId)) && setName) {
-          acc[Number(chainId)] = setName;
-        }
-        return acc;
-      },
-      {} as Record<number, string>,
-    );
-
-// Devnet deployments
-const DEVNETS_MAP = {
-  'hoodi-public-1': hoodiPublic1Set,
-} as Record<string, NetworkConfig>;
 
 // Main deployments
 const NETWORKS_MAP = {
@@ -46,16 +19,6 @@ const NETWORKS_MAP = {
 } as Record<string, NetworkConfig>;
 
 export const getNetworkConfig = (chain: CHAINS): NetworkConfig | undefined => {
-  const overridedSetName = DEVNET_OVERRIDES[chain];
-
-  if (overridedSetName) {
-    invariant(
-      overridedSetName in DEVNETS_MAP,
-      `DEVNETS_MAP doesn't contain the override set "${overridedSetName}" for chainId: ${chain}`,
-    );
-    return DEVNETS_MAP[overridedSetName];
-  }
-
   return NETWORKS_MAP[chain];
 };
 
