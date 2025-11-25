558955330db29a21c928bfe6528353dbc747b716
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2025-11-25 12:20:15 +0300

    feat: replace mintStETH by mintShares

--- features/adjustment/mint/form/mint-form-context/use-mint.ts
+++ features/adjustment/mint/form/mint-form-context/use-mint.ts
@@ -1,30 +1,53 @@
 import invariant from 'tiny-invariant';
 import { useCallback } from 'react';
 
-import { useSendTransaction, withSuccess } from 'modules/web3';
-
-import { useReportCalls, useVault, vaultTexts } from 'modules/vaults';
-
+import { useLidoSDK, useSendTransaction, withSuccess } from 'modules/web3';
+import {
+  getStEthContract,
+  useReportCalls,
+  useVault,
+  vaultTexts,
+} from 'modules/vaults';
 import { GoToVault } from 'modules/vaults/components/go-to-vault';
-import { MintFormValidatedValues } from '../types';
+
+import { useMintData } from './use-mint-data';
+import type { MintFormValidatedValues } from '../types';
 
 export const useMint = () => {
   const { activeVault } = useVault();
+  const { publicClient } = useLidoSDK();
   const prepareReportCalls = useReportCalls();
+  const { mintableQuery } = useMintData();
   const { sendTX, ...rest } = useSendTransaction();
+  const maxMintableShares = mintableQuery.data?.maxMintableShares;
 
   return {
     mint: useCallback(
       async ({ amount, recipient, token }: MintFormValidatedValues) => {
         invariant(activeVault, '[useMint] activeVault is undefined');
+        invariant(publicClient, '[useMint] publicClient is undefined');
+        invariant(
+          maxMintableShares,
+          '[useMint] maxMintableShares is undefined',
+        );
 
+        const stethContract = getStEthContract(publicClient);
         const loadingActionText = vaultTexts.actions.mint.loading(token);
         const mainActionCompleteText = vaultTexts.actions.mint.completed(token);
 
+        let txAmount = amount;
+        if (token === 'stETH') {
+          const sharesAmount = await stethContract.read.getSharesByPooledEth([
+            amount,
+          ]);
+          const diff = maxMintableShares - sharesAmount;
+          txAmount = diff === 1n ? sharesAmount + 1n : sharesAmount;
+        }
+
         const mintCall = {
           ...activeVault.dashboard.encode[
-            token === 'stETH' ? 'mintStETH' : 'mintWstETH'
-          ]([recipient, amount]),
+            token === 'stETH' ? 'mintShares' : 'mintWstETH'
+          ]([recipient, txAmount]),
           loadingActionText,
         };
 
@@ -40,7 +63,13 @@ export const useMint = () => {
 
         return success;
       },
-      [activeVault, prepareReportCalls, sendTX],
+      [
+        activeVault,
+        prepareReportCalls,
+        sendTX,
+        publicClient,
+        maxMintableShares,
+      ],
     ),
     ...rest,
   };
