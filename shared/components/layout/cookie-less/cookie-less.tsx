fa37d6a37e2c86942f784a0a8faf72e75b5afe4c
Dmitriy Evtifeev <evtifeev.dmitry@gmail.com>
2026-01-22 14:01:45 +0300

    fix: refactor privacy notice and term of use links

--- shared/components/layout/cookie-less/cookie-less.tsx
+++ shared/components/layout/cookie-less/cookie-less.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 
+import { config } from 'config';
+
 import { CookieText, CookieWrapper } from './styles';
+
+const link = `${config.rootOrigin}/privacy-notice`;
 
 export const CookieLess = () => {
   return (
@@ -9,11 +13,7 @@ export const CookieLess = () => {
         Your privacy matters. We use cookieless analytics and collect only
         anonymized data for improvements. Cookies are used for functionality
         only. For more info read{' '}
-        <Link
-          target="_blank"
-          href="https://lido.fi/privacy-notice"
-          rel="noopener noreferrer"
-        >
+        <Link target="_blank" href={link} rel="noopener noreferrer">
           Privacy Notice
         </Link>
         .
