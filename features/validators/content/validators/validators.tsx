e83cfad9530867eaf45d4490e9920f76c6c13cb8
itaven <vlad.k@lido.fi>
2026-05-07 14:46:44 +0500

    feat: validators data-testid(1)

--- features/validators/content/validators/validators.tsx
+++ features/validators/content/validators/validators.tsx
@@ -11,7 +11,7 @@ export const Validators = () => {
   const { isLoading, validators, isParamsDefault, isError } = useValidators();
 
   return (
-    <Section>
+    <Section data-testid="validators-list">
       <CircleLoader
         isLoading={isLoading}
         showBy={!((validators?.length === 0 && isParamsDefault) || isError)}
